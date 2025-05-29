7e843a449fb260557527c9f3aa4ddbca51792877
Florido-hub <floridodinizz@gmail.com>
2025-05-29 10:47:37 -0300

    integra JavaFX à estrutura do projeto

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,11 +1,18 @@
 package application;
 import dao.ProdutoDAO;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import model.Product;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
         ProdutoDAO dao = new ProdutoDAO();
         Product p = new Product(100000, "caneta", 2.0, "azul", 100);
         dao.inserir(p);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
