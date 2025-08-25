a039de8c6101bda2a1010258fdbc9456b7ef197a
Florido-hub <floridodinizz@gmail.com>
2025-08-25 10:03:13 -0300

    fix: corrige erros no codigo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,4 @@
 INSERT INTO tb_product (name, price, quantity, category) VALUES ('Geladeira', 1500.00, 100, 'eletrodomestico');
-INSERT INTO tb_product (name, price, quantity, category) VALUES ('Notebook', 3200.50, 50, 'eletrodomestico');
 INSERT INTO tb_product (name, price, quantity, category) VALUES ('Liquidificador', 850.75, 75, 'eletrodomestico');
 INSERT INTO tb_product (name, price, quantity, category) VALUES ('Airfryer', 300.20, 80, 'eletrodomestico');
 INSERT INTO tb_product (name, price, quantity, category) VALUES ('Roteador Wi-Fi', 250.00, 120, 'eletrodomestico');
@@ -10,3 +9,4 @@ INSERT INTO tb_product (name, price, quantity, category) VALUES ('Cama Casal', 1
 
 INSERT INTO tb_product (name, price, quantity, category) VALUES ('Fone de Ouvido', 75.50, 250, 'lazer');
 INSERT INTO tb_product (name, price, quantity, category) VALUES ('Bicicleta', 180.00, 90, 'lazer');
+INSERT INTO tb_product (name, price, quantity, category) VALUES ('churrasqueira', 180.00, 90, 'lazer');
